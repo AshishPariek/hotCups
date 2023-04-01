0d5173323091caff677464eb51d44fee3bf6998b
Ashish Kumar Pareek <115872715+AshishPariek@users.noreply.github.com>
2023-04-01 11:56:59 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+
+const app = express();
+const port = 4500;
+
+app.use(cors());
+app.use(express.json());
+
+const mydb = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hotcup_db",
+});
+
+app.get("/hotcup/shops", (req, res) => {
+  const sqlGet = "select * from coffee_shop";
+  mydb.query(sqlGet, (err, result) => {
+    err ? console.log("Error : ", err) : console.log("ok");
+    res.send(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server Running at Port : ", port);
+});
